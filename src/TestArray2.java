3e3b5a08cdb219ab6b8e80287ae6ec88d5ecd07d
myR3po <>
2017-11-12 07:22:49 +0000

    Fix the method duplicate
    Rename the method doublon in duplicate

--- src/TestArray2.java
+++ src/TestArray2.java
@@ -15,45 +15,50 @@ public class TestArray2 {
 	private void IndexOf15() {
 		for (int i = 0; i < array.length; i++) {
 			if (array[i] == 15) {
+				System.out.println("[Programmer way] index is " + i);
 				System.out.println("Index is " + (i + 1));
 			}
 		}
 	}
 
-	public int doublon() {
+	public int duplicate(int[] anArray) {
 
-//		TODO (ME) not  good
-		int nbDoublon = 0 ;
+		int[] theClone = anArray.clone();
+		int arrayLength = anArray.length;
+		int nbDuplicate = 0 ;
 		int temp = 0;
-		for (int i = 0 ; i < array.length ; i++) {
-			for (int j = 0 ; j < array.length ; j++) {
-				
-				if((i != j)) {
-						if(array[i] == array[j]) {
-					temp++;
+		/*
+		 * With this array it's work but with an other
+		 * we need to be careful with the values.
+		 */
+		int check = Integer.MAX_VALUE;
+		for (int i = 0 ; i < arrayLength ; i++) {
+			if(theClone[i] != check){
+				for (int j = 0 ; j < arrayLength ; j++) {
+					if((i != j) && (theClone[j] != check) && theClone[i] == theClone[j]) {
+						theClone[j] = check;
+						temp++;
+					}
 				}
-						}
+				
+				if(temp >= 1 ) {
+					nbDuplicate++ ;
+					theClone[i] = check;
+					temp = 0;
+				}
 			}
-			
-			if(temp >= 2 ) {
-				nbDoublon++ ;
-			}
-			temp = 0;
 		}
-		return nbDoublon;
+		return nbDuplicate;
 	}
 
 	public void makeExo2() {
 		System.out.println("The mean is " + mean());
 		IndexOf15();
-		System.out.println("There are " + doublon());
+		System.out.println("There are " + duplicate(array) + " duplicate(s).");
 	}
 
 	public static void main(String[] args) {
-
 		TestArray2 testArray2 = new TestArray2();
 		testArray2.makeExo2();
-
 	}
-
 }
